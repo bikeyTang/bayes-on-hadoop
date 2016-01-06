34d7600fb19acef4358d82d21ced494ec04a3f48
tang <tjpu_tangkun@126.com>
2016-01-06 16:21:16 +0800

    add test2

--- testbayes/src/Test.java
+++ testbayes/src/Test.java
@@ -54,32 +54,84 @@ public class Test {
 	/*
 	 * 把从文件中读取到的map转化成相应的map
 	 */
-	public static Map<Object,Object> transMap(Map<Object,String> map){
-		Map<Object,Object> m=new HashMap<Object,Object>();
-		for(Map.Entry<Object, String> val:map.entrySet()){
+	public static Map<Object,Integer> transMap(Map<Integer, String> cn_map){
+		Map<Object,Integer> m=new HashMap<Object,Integer>();
+		for(Map.Entry<Integer, String> val:cn_map.entrySet()){
 			StringTokenizer st=new StringTokenizer(val.getValue());
 			String ikey=st.nextToken();
 			String ivalue="";
-			if(st.countTokens()>2){
-				while(st.hasMoreTokens()){
-					ivalue+=st.nextToken()+ " ";
-				}
+			if(st.countTokens()>=2){
+				String tvalue=st.nextToken();
 				HashMap<Object,String> hm=new HashMap<Object,String>();
-				hm.put(ikey, ivalue);
-				m.put(ikey, transMap(hm));
+				hm.put(ikey, tvalue);
+				while(st.hasMoreTokens()){
+					ivalue+=st.nextToken();
+				}
+				m.put(hm, Integer.parseInt(ivalue));
 			}else{
 				ivalue=st.nextToken();
-				m.put(ikey, ivalue);
+				m.put(ikey, Integer.parseInt(ivalue));
 			}
 		}
 		return m;
 	}
-	public static double getLogProbability(Map<Object,Object> condm,Map<Object,Object>  countm,HashSet<String> set){
+	/**
+	 * 计算概率
+	 * @param cwn
+	 * @param cn
+	 * @param set
+	 * @return
+	 */
+	public static Map<Map<String,String>,Double> getCondLogProbability(Map<Map<String,String>,Integer> cwn,Map<String,Integer>  cn,HashSet<String> set){
 		int v_length=set.size();
+		Map<Map<String,String>,Double> mp=new HashMap<Map<String,String>,Double>();
 		
-		return 0;
+		for(Map.Entry<Map<String,String>, Integer> val:cwn.entrySet()){
+			for(Map.Entry<String, String> sval:val.getKey().entrySet()){
+				int vt=val.getValue()+1;
+				int vt_sum=cn.get(sval.getKey())+v_length; //类别词数+词典向量长度
+				double p=Math.log(vt)-Math.log(vt_sum);
+				mp.put(val.getKey(),p);
+			}
+		}
+		System.out.println("changed:"+mp.size());
+		return mp;
 	}
-	public static void main(String[] args) {
+	public static String predictP(Map<Map<String,String>,Double> condp,Map<String,Integer> doc,HashSet<String> set,Map<String,Integer> cn){
+		String c=null;
+		for(Map.Entry<String, Integer> cn_val:cn.entrySet()){
+//			Iterator<String> itr=set.iterator();
+//			while(itr.hasNext()){
+//				
+//			}
+			String c_key=cn_val.getKey();
+			for(Map.Entry<String, Integer> doc_val:doc.entrySet()){
+			}
+		}
+		return c;
+	}
+	public static void main(String[] args) throws IOException {
+		String root_dir="/home/hadoop/桌面/";
+		String cn_file=root_dir+"C-N";
+		String cwn_file=root_dir+"((c,w),n)";
+		String v_file=root_dir+"v";
+		Map<Integer,String> cn_map=readFileByLine(cn_file);
+		Map<Integer,String> cwn_map=readFileByLine(cwn_file);
+		Map<Integer,String> v_map=readFileByLine(v_file);
+		HashSet<String> v_set=getVectory(v_map);
+		Map<Object,Integer> cn_o=null,cwn_o=null;
+		cn_o=transMap(cn_map);
+		cwn_o=transMap(cwn_map);
+		Map<String,Integer> cn=new HashMap<String,Integer>();
+		Map<Map<String,String>,Integer> cwn=new HashMap<Map<String,String>,Integer>();
+		for(Map.Entry<Object,Integer> val:cn_o.entrySet()){
+			cn.put(val.getKey().toString(), val.getValue());
+		}
+		for(Map.Entry<Object, Integer> val:cwn_o.entrySet()){
+			cwn.put((Map<String,String>)val.getKey(), val.getValue());
+		}
+		Map<Map<String,String>,Double> imp=new HashMap<Map<String,String>,Double>();
+		imp=getCondLogProbability(cwn, cn, v_set);
 		
 	}
 
